ef5237c4639dab40359dc5bfbb5f25078865e1ca
Azeez Abass <abassazeez6@gmail.com>
2023-02-21 18:17:22 -0700

    fix: implement Resolve

--- src/Resolve.cpp
+++ src/Resolve.cpp
@@ -1,34 +1,22 @@
+#include <algorithm>
+
 #include "plugin.hpp"
 
+
 struct Resolve : Module {
+	static const int NUM_TRIGIN = 6;
+	int selected = 0;
+
 	enum ParamIds {
 		NUM_PARAMS
 	};
 	enum InputIds {
-		TRIG1_INPUT,
-		IN1A_INPUT,
-		IN1B_INPUT,
-		TRIG2_INPUT,
-		IN2A_INPUT,
-		IN2B_INPUT,
-		TRIG3_INPUT,
-		IN3A_INPUT,
-		IN3B_INPUT,
-		TRIG4_INPUT,
-		IN4A_INPUT,
-		IN4B_INPUT,
-		TRIG5_INPUT,
-		IN5A_INPUT,
-		IN5B_INPUT,
-		TRIG6_INPUT,
-		IN6A_INPUT,
-		IN6B_INPUT,
-		TRIG7_INPUT,
-		IN7A_INPUT,
-		IN7B_INPUT,
-		TRIG8_INPUT,
-		IN8A_INPUT,
-		IN8B_INPUT,
+		TRIG0_INPUT, IN0A_INPUT, IN0B_INPUT,
+		TRIG1_INPUT, IN1A_INPUT, IN1B_INPUT,
+		TRIG2_INPUT, IN2A_INPUT, IN2B_INPUT,
+		TRIG3_INPUT, IN3A_INPUT, IN3B_INPUT,
+		TRIG4_INPUT, IN4A_INPUT, IN4B_INPUT,
+		TRIG5_INPUT, IN5A_INPUT, IN5B_INPUT,
 		NUM_INPUTS
 	};
 	enum OutputIds {
@@ -38,14 +26,12 @@ struct Resolve : Module {
 		NUM_OUTPUTS
 	};
 	enum LightIds {
+		LED0_LIGHT,
 		LED1_LIGHT,
 		LED2_LIGHT,
 		LED3_LIGHT,
 		LED4_LIGHT,
 		LED5_LIGHT,
-		LED6_LIGHT,
-		LED7_LIGHT,
-		LED8_LIGHT,
 		NUM_LIGHTS
 	};
 
@@ -53,8 +39,47 @@ struct Resolve : Module {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 	}
 
+	float getInputPolyMaxVoltage(InputIds id) {
+		const int input_channels = inputs[id].getChannels();
+		const float* input_voltages = inputs[id].getVoltages();
+		const float input_max_voltage = *std::max_element(input_voltages, input_voltages+input_channels);
+		return input_max_voltage;
+	}
+
 	void process(const ProcessArgs& args) override {
 
+		float max_trig_voltage = 0;
+		float* output_voltages[3] = {nullptr, nullptr, nullptr};
+		int selected_channels[3] = {0, 0, 0};
+
+		for(int tid=0; tid<NUM_TRIGIN; tid++) {
+			const float trig_max_poly_voltage = this->getInputPolyMaxVoltage((InputIds)(tid*3));
+			if ( trig_max_poly_voltage && (trig_max_poly_voltage >= max_trig_voltage)) {
+				max_trig_voltage = trig_max_poly_voltage;
+				selected = tid;
+			} 
+		}
+		
+		for (int i = 0; i < 3; i++) {
+			selected_channels[i] = inputs[selected*3+i].getChannels();
+		};
+		
+		outputs[TRIG_OUTPUT].setChannels(selected_channels[0]);
+		outputs[OUTA_OUTPUT].setChannels(selected_channels[1]);
+		outputs[OUTB_OUTPUT].setChannels(selected_channels[2]);
+		
+		for (int i = 0; i < 3; i++) {
+			output_voltages[i] = inputs[selected*3+i].getVoltages();
+		}
+
+		outputs[TRIG_OUTPUT].writeVoltages(output_voltages[0]);
+		outputs[OUTA_OUTPUT].writeVoltages(output_voltages[1]);
+		outputs[OUTB_OUTPUT].writeVoltages(output_voltages[2]);
+
+
+		for (int tid=0; tid < NUM_TRIGIN; tid++) {
+			lights[tid].setBrightness((float)(tid == selected));
+		}
 	}
 };
 
@@ -69,43 +94,17 @@ struct ResolveWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.25, 47.25)), module, Resolve::TRIG1_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(16.25, 47.25)), module, Resolve::IN1A_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(25.0, 47.25)), module, Resolve::IN1B_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.25, 54.75)), module, Resolve::TRIG2_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(16.25, 54.75)), module, Resolve::IN2A_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(25.0, 54.75)), module, Resolve::IN2B_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.25, 62.25)), module, Resolve::TRIG3_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(16.25, 62.25)), module, Resolve::IN3A_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(25.0, 62.25)), module, Resolve::IN3B_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.25, 69.75)), module, Resolve::TRIG4_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(16.25, 69.75)), module, Resolve::IN4A_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(25.0, 69.75)), module, Resolve::IN4B_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.25, 77.25)), module, Resolve::TRIG5_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(16.25, 77.25)), module, Resolve::IN5A_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(25.0, 77.25)), module, Resolve::IN5B_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.25, 84.75)), module, Resolve::TRIG6_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(16.25, 84.75)), module, Resolve::IN6A_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(25.0, 84.75)), module, Resolve::IN6B_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.25, 92.25)), module, Resolve::TRIG7_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(16.25, 92.25)), module, Resolve::IN7A_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(25.0, 92.25)), module, Resolve::IN7B_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.25, 99.75)), module, Resolve::TRIG8_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(16.25, 99.75)), module, Resolve::IN8A_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(25.0, 99.75)), module, Resolve::IN8B_INPUT));
-
+		for (int i = 0; i < Resolve::NUM_TRIGIN; i++) {
+			const int y = 40.00 + 11.0 * i;
+			addInput(createInputCentered<PJ301MPort>(mm2px(Vec(6.25, y)), module, i*3));
+			addInput(createInputCentered<PJ301MPort>(mm2px(Vec(16.25, y)), module, i*3 + 1));
+			addInput(createInputCentered<PJ301MPort>(mm2px(Vec(25.0, y)), module, i*3 +2));
+			addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(11.25, y+3.0)), module, i));
+		}
+		
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(6.25, 107.25)), module, Resolve::TRIG_OUTPUT));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(16.25, 107.25)), module, Resolve::OUTA_OUTPUT));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(25.0, 107.25)), module, Resolve::OUTB_OUTPUT));
-
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(11.25, 48.5)), module, Resolve::LED1_LIGHT));
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(11.25, 56.0)), module, Resolve::LED2_LIGHT));
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(11.25, 63.5)), module, Resolve::LED3_LIGHT));
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(11.25, 71.0)), module, Resolve::LED4_LIGHT));
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(11.25, 78.5)), module, Resolve::LED5_LIGHT));
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(11.25, 86.0)), module, Resolve::LED6_LIGHT));
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(11.25, 93.5)), module, Resolve::LED7_LIGHT));
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(11.25, 101.0)), module, Resolve::LED8_LIGHT));
 	}
 };
 
